497c2a1cac2dabba76b11e21fdbaecff8ab63680
Henning Kerstan <mail@henningkerstan.de>
2021-08-21 14:13:10 +0200

    add naming conventions for variables

--- index.js
+++ index.js
@@ -21,6 +21,34 @@ module.exports = {
   ],
 
   rules: {
-    curly: 'error', // require blocks to be in curly braces
+    // require blocks to be in curly braces
+    curly: 'error',
+
+    // define naming conventions
+    '@typescript-eslint/naming-convention': [
+      'error',
+
+      // default is camelCase without leading/trailing underscores
+      {
+        selector: 'default',
+        format: ['camelCase'],
+        leadingUnderscore: 'forbid',
+        trailingUnderscore: 'forbid',
+      },
+
+      // private members may have a leading underscore
+      {
+        selector: 'memberLike',
+        modifiers: ['private'],
+        format: ['camelCase'],
+        leadingUnderscore: 'allow',
+      },
+
+      // default for types is PascalCase
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+    ],
   },
 }
